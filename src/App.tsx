0b851391f96089dd9cd07bd269597cb6ff2bfed5
Jeff Jia <jeff.jia@we-are-mea.com>
2026-01-29 11:31:08 +1300

    added simple styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,17 @@
 import { useEffect, useState } from 'react'
 import './App.css'
-
 export default function App() {
     // create timer object with id
     const [timers, setTimers] = useState([{ id: Date.now() }])
-
     function addTimer() {
         const timerArrayCopy = timers.slice()
         timerArrayCopy.push({ id: Date.now() })
         setTimers(timerArrayCopy)
     }
-
     function deleteTimer(id: number) {
         const updatedArray = timers.filter((item) => item.id !== id)
         setTimers(updatedArray)
     }
-
     // returns an add button and loops and renders entire timer grid
     return (
         <div>
@@ -31,50 +27,44 @@ export default function App() {
         </div>
     )
 }
-
 function ModuleBlock({ onClick }: { onClick: () => void }) {
     const [seconds, setSeconds] = useState(0)
     const [isRunning, setIsRunning] = useState(false)
     const [isEditing, setIsEditing] = useState(false)
     const [name, setName] = useState('Average Timer')
-
     function formatTime(sec: number) {
         const hours = Math.floor(sec / 3600)
         const minutes = Math.floor((sec % 3600) / 60)
         const seconds = sec % 60
-
         // uses 0 as filler/padding
         return `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`
     }
-
     function startStopButtonOnClick() {
         setIsRunning(!isRunning)
     }
-
+    function resetTimer() {
+        setIsRunning(false)
+        setSeconds(0)
+    }
     // timing effect
     useEffect(() => {
         if (!isRunning) {
             return
         }
-
         function addSecond() {
             setSeconds((prev) => prev + 1)
         }
-
         // skip one second wait after btn press
         addSecond()
-
         // runs add second every second
         const interval = setInterval(addSecond, 1000)
-
         // stop setInterval from firing
         return () => {
             clearInterval(interval)
         }
     }, [isRunning])
-
     const formattedTime = formatTime(seconds)
-
+    // render element depending on editing status
     let nameElement
     if (isEditing) {
         nameElement = (
@@ -93,19 +83,24 @@ function ModuleBlock({ onClick }: { onClick: () => void }) {
     } else {
         nameElement = <h3 onClick={() => setIsEditing(true)}>{name}</h3>
     }
-
     return (
         <div className="module-block">
-            <button className="delete-btn" onClick={onClick}></button>
-            {nameElement}
-            <div className="time-container">
-                <h3 className="time-text">{formattedTime}</h3>
+            <div className="x-container">
+                <button className="delete-btn" onClick={onClick}>
+                    x
+                </button>
+            </div>
+            <div id="functionality-container">
+                {nameElement}
+                <div className="time-container">
+                    <h3 className="time-text">{formattedTime}</h3>
+                </div>
                 <GeneralButton btnText={isRunning ? 'Stop' : 'Start'} onClick={startStopButtonOnClick} />
+                <GeneralButton btnText="Reset" onClick={resetTimer} />
             </div>
         </div>
     )
 }
-
 function GeneralButton({ btnText, onClick }: { btnText: string; onClick: () => void }) {
     return (
         <button className="module-btn" onClick={onClick}>
